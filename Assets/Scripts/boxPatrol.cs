9278806fa5bda0f0e21beb5faf6cfdc12758bcad
lucav88 <101470194+lucaseavila@users.noreply.github.com>
2022-03-13 20:34:05 -0300

    Commit-15
    Nivel2

--- Assets/Scripts/boxPatrol.cs
+++ Assets/Scripts/boxPatrol.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,13 +14,14 @@ public class boxPatrol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, movePoint[nextStep].position, moveSpeed * Time.deltaTime);
+       
+   transform.position = Vector2.MoveTowards(transform.position, movePoint[nextStep].position, moveSpeed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position,movePoint[nextStep].position) < minDistance)
         {
